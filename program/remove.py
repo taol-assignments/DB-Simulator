647b0c7ed7876e820214497ab23415d68fc38586
taoliu <taol@mun.ca>
2019-11-11 22:09:05 -0330

    Remove tree function.

--- program/remove.py
+++ program/remove.py
@@ -7,8 +7,36 @@ data_path = os.path.join(base_path, "data")
 index_path = os.path.join(base_path, "index")
 
 
+def _do_remove_tree(page_name, page_list):
+    page_path = os.path.join(index_path, page_name)
+
+    page = json.load(open(page_path, "r"))
+    if page[0] == "I":
+        for i in range(0, len(page[2]), 2):
+            _do_remove_tree(page[2][i], page_list)
+
+    page_list.append(page_name)
+    os.unlink(page_path)
+
+
 def removeTree(rel, att):
-    pass
+    directory_path = os.path.join(index_path, "directory.txt")
+
+    page_list = []
+
+    directory = json.load(open(directory_path, "r"))
+    for i, root in enumerate(directory):
+        if root[0] == rel and root[1] == att:
+            _do_remove_tree(root[2], page_list)
+            directory = directory[:i] + directory[i + 1:]
+            json.dump(directory, open(directory_path, "w"))
+            break
+
+    page_pool_path = os.path.join(index_path, "pagePool.txt")
+    page_pool = json.load(open(page_pool_path, "r"))
+    page_pool += page_list
+
+    json.dump(page_pool, open(page_pool_path, "w"))
 
 
 def removeTable(rel):
